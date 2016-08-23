c07a188135280113bfbe4d2b3b7d8f3613dde0be
Jin Chen <chenjin10@gmail.com>
2016-08-23 18:14:11 +1200

    Initial commit. DB was auto generated successfully.

---
+++ EF6/DAL/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using EF6.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace EF6.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext") { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Course { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
